a3af371378ff887b87108db328367000558b6025
Lucas Nascimento <lucns.dev@gmail.com>
2026-06-22 22:13:29 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	var mainButton = document.getElementById("button");
+	mainButton.addEventListener('click', e => {
+	    window.location.href = "https://github.com/lucnsdev?tab=repositories";
+	});
+}
+
+// Developed by @lucns
